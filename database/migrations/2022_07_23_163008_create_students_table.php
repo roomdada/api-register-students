e8397159c2be0f0fc1f146c2abd162255486a807
Roomdada <dsieroger@gmail.com>
2022-07-23 16:51:45 +0000

    fix

--- database/migrations/2022_07_23_163008_create_students_table.php
+++ database/migrations/2022_07_23_163008_create_students_table.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use App\Models\Level;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
