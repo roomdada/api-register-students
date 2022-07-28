16f8f5f0ae902fad4b54c4feecb6bc53065aec66
Roomdada <dsieroger@gmail.com>
2022-07-28 12:44:52 +0000

    hotfix: correction de seeder

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -2,20 +2,37 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Category;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class CategorySeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        \App\Models\Category::factory()->count(10)->create()->each(function ($category) {
-            $category->articles()->saveMany(\App\Models\Article::factory()->count(10)->make());
-        });
+  /**
+   * Run the database seeds.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    Category::create(['title' => 'Marketing']);
+    Category::create(['title' => 'Web']);
+    Category::create(['title' => 'Hacking']);
+    Category::create(['title' => 'Motivation']);
+    $faker = \Faker\Factory::create();
+    for ($i = 0; $i < 10; $i++) {
+      \App\Models\Article::create([
+        'title' => $faker->sentence,
+        'content' => $faker->paragraph,
+        'image' => $faker->imageUrl,
+        'category_id' => rand(1, 4),
+      ]);
     }
+
+    return 1;
+
+    \App\Models\Category::factory()->count(10)->create()->each(function ($category) {
+      $category->articles()->saveMany(\App\Models\Article::factory()->count(10)->make());
+    });
+  }
 }
